ad4cb53a2314121e29dc0d9c408e5681870a59ff
Mykhailo Bubnov <bubm02@vse.cz>
2022-10-06 10:31:31 +0200

    Comments added In QuizController

--- Web Quiz Engine/task/src/engine/QuizController.java
+++ Web Quiz Engine/task/src/engine/QuizController.java
@@ -32,6 +32,15 @@ import java.sql.Timestamp;
 import java.util.*;
 
 
+/**
+ * This class is the main controller of the application. It handles all the requests and responses.
+ * It also handles the database operations.
+ * It uses the QuizService, QuizCompletionService and UserService classes to access the database.
+ * It has 7 endpoints, 1(registration) is available for everyone, 6 are available for authenticated users.
+ *
+ * @author Mykhailo Bubnov
+ * @version 0.1
+ */
 @RestController
 public class QuizController {
 
@@ -42,6 +51,7 @@ public class QuizController {
     UserService userService;
 
     PasswordEncoder encoder;
+
 
     @Autowired
     public void setEncoder(PasswordEncoder encoder) {
@@ -54,6 +64,15 @@ public class QuizController {
         this.quizCompletionService = quizCompletionService;
     }
 
+
+    /**
+     * This method handles the registration request.
+     * It checks if the user already exists in the database and if email and password are valid.
+     * If the user does not exist and password and email are valid, it creates a new user and saves it to the database.
+     * If something doesn't fit, it throws a BadRegistrationRequestException.
+     * @param user email and password
+     * @return ResponseEntity with the status code 200 and the message "You have successfully registered".
+     */
     @PostMapping(path="/api/register", consumes = "application/json")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
         if (user.getEmail().matches(".*@.*\\..*")
@@ -72,18 +91,16 @@ public class QuizController {
     }
 
 
-
-
-
-
     /*
-
-    Quiz operations
-
+      Quiz Operations
      */
 
-    /*
-    Create a new quiz
+
+    /**
+     * This mapping allows getting a certain quiz with its id.
+     * Throws a QuizNotFoundException if the quiz with the given id does not exist.
+     * @param id, entered as a path variable
+     * @return ResponseEntity with the status code 200 and the quiz in the body.
      */
     @GetMapping(value = "/api/quizzes/{id}", produces = "application/json")
     public QuizJPAEntity getQuiz(@PathVariable int id) {
@@ -94,8 +111,12 @@ public class QuizController {
     }
 
 
-
-
+    /**
+     * This mapping allows getting all the quizzes from the database.
+     * Quizzes are organised in pages, each page has 10 quizzes.
+     * @param page page number
+     * @return ResponseEntity with the status code 200 and the list of quizzes in the body.
+     */
     @GetMapping(value = "/api/quizzes", produces = "application/json")
     public ResponseEntity<Page<QuizJPAEntity>> getQuizzes(
             @RequestParam(defaultValue = "0") int page) {
@@ -108,6 +129,14 @@ public class QuizController {
         return new ResponseEntity<>(pagedResult, HttpStatus.OK);
     }
 
+    /**
+     * This mapping allows getting all quiz completion entities from the database for authenticated user.
+     * They contain the date of completion and the id of the quiz.
+     * Quiz completion entities are organised in pages, each page has 10 quiz completion entities.
+     * @param page page number
+     * @param auth authentication object, automapped.
+     * @return ResponseEntity with the status code 200 and the list of quiz completion entities in the body.
+     */
     @GetMapping(value = "/api/quizzes/completed", produces = "application/json")
     public ResponseEntity<Page<QuizCompletionEntity>> getQuizzescompletion(
             @RequestParam(defaultValue = "0") int page,
@@ -124,8 +153,20 @@ public class QuizController {
     }
 
 
-
-
+    /**
+     * This mapping allows creating a quiz.
+     * It consumes a quiz object in the body of the request.
+     * Sample request:
+     * {
+     *     "title": "The Java Logo",
+     *     "text": "What is depicted on the Java logo?",
+     *     "options": ["Robot", "Tea leaf", "Cup of coffee", "Bug"],
+     *     "answer": [2,3]
+     * }
+     * @param quiz quiz object in the body of the request
+     * @param auth authentication object, automapped.
+     * @return ResponseEntity with the status code 200 and the created quiz in the body.
+     */
     @PostMapping(value = "/api/quizzes", consumes = "application/json", produces = "application/json")
     public QuizJPAEntity createQuiz (@Valid @RequestBody QuizJPAEntity quiz, Authentication auth) {
         if (quiz.getAnswer() == null) {
@@ -137,7 +178,13 @@ public class QuizController {
     }
 
 
-
+    /**
+     * This mapping allows solving a quiz.
+     * @param id id of the quiz as the path variable.
+     * @param answerObject object with the answer in the body of the request, sample : {"answer": [2,3]}
+     * @param auth authentication object, automapped.
+     * @return ResponseEntity with either the congratulation message or the try again message.
+     */
     @PostMapping(value = "/api/quizzes/{id}/solve", produces = "application/json")
     public ServerFeedback solveQuiz(@PathVariable int id, @RequestBody(required = false) Answer answerObject, Authentication auth) {
         int[] answer = answerObject.getAnswer();
@@ -160,6 +207,13 @@ public class QuizController {
         throw new QuizNotFoundException("Quiz with this id is not found");
     }
 
+    /**
+     * This mapping allows deleting a quiz with a certain id.
+     * Only the user that created the quiz can delete it.
+     * @param id id of the quiz as the path variable.
+     * @param auth authentication object, automapped.
+     * @return ResponseEntity with the status code 204.
+     */
     @DeleteMapping(value = "/api/quizzes/{id}")
     public ResponseEntity<String> deleteQuiz(@PathVariable int id, Authentication auth) {
         UserJPAEntity user = (UserJPAEntity) auth.getPrincipal();
@@ -174,7 +228,10 @@ public class QuizController {
     }
 
 
-
+    /**
+     * Bean that generates a password encoder.
+     * It is used in the registration process.
+     */
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
