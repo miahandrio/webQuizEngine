d6516e77f877ecf55e9272e42222675e956e5696
Mykhailo Bubnov <bubm02@vse.cz>
2022-10-18 16:54:35 +0200

    Added tests for getQuiz and getQuizzes methods

--- Web Quiz Engine/task/test/tests/engine/QuizControllerTest.java
+++ Web Quiz Engine/task/test/tests/engine/QuizControllerTest.java
@@ -4,11 +4,15 @@ package tests.engine;
 import engine.JSONEntities.User;
 import engine.QuizController;
 import engine.database.quizcompletiontable.QuizCompletionService;
+import engine.database.quiztable.QuizJPAEntity;
 import engine.database.quiztable.QuizService;
 import engine.database.usertable.UserService;
 import engine.exceptions.BadRegistrationRequestException;
+import engine.exceptions.QuizNotFoundException;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -49,7 +53,7 @@ public class QuizControllerTest {
      *
      */
     @Test
-    public void testRegisterUserValid_Expect200_1() {
+    public void testRegisterUser_Valid_Expect200_1() {
         User validUser = new User("test@google.com", "qwerty");
         HttpStatus expectedStatus = HttpStatus.OK;
 
@@ -57,7 +61,7 @@ public class QuizControllerTest {
     }
 
     @Test
-    public void testRegisterUserValid_Expect200_2() {
+    public void testRegisterUser_Valid_Expect200_2() {
         User validUser = new User("@.", "qwerty");
         HttpStatus expectedStatus = HttpStatus.OK;
 
@@ -73,7 +77,7 @@ public class QuizControllerTest {
      *
      */
     @Test
-    public void testRegisterUserWithNotMatchingEmail_ExpectException1() {
+    public void testRegisterUser_WithNotMatchingEmail_ExpectException1() {
         User invalidUser = new User("test", "qwerty");
 
         assertThrows(
@@ -82,7 +86,7 @@ public class QuizControllerTest {
     }
 
     @Test
-    public void testRegisterUserWithNotMatchingEmail_ExpectException2() {
+    public void testRegisterUser_WithNotMatchingEmail_ExpectException2() {
         User invalidUser = new User("test@", "qwerty");
 
         assertThrows(
@@ -91,7 +95,7 @@ public class QuizControllerTest {
     }
 
     @Test
-    public void testRegisterUserWithNotMatchingEmail_ExpectException3() {
+    public void testRegisterUser_WithNotMatchingEmail_ExpectException3() {
         User invalidUser = new User("test.", "qwerty");
 
         assertThrows(
@@ -100,7 +104,7 @@ public class QuizControllerTest {
     }
 
     @Test
-    public void testRegisterUserWithNotMatchingEmail_ExpectException4() {
+    public void testRegisterUser_WithNotMatchingEmail_ExpectException4() {
         User invalidUser = new User(".@", "qwerty");
 
         assertThrows(
@@ -108,4 +112,48 @@ public class QuizControllerTest {
                 () -> quizController.registerUser(invalidUser));
     }
 
+
+    /**
+     *
+     *
+     * getQuiz test
+     *
+     *
+     */
+
+    @Test
+    public void testGetQuiz_ExpectReturns200() {
+        Mockito.when(quizService.isQuizExist(1)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(1)).thenReturn(new QuizJPAEntity());
+
+        assertEquals(HttpStatus.OK, quizController.getQuiz(1).getStatusCode());
+    }
+
+    @Test
+    public void testGetQuiz_ExpectException() {
+        Mockito.when(quizService.isQuizExist(1)).thenReturn(false);
+
+        assertThrows(QuizNotFoundException.class,
+                () -> quizController.getQuiz(1));
+    }
+
+
+    /**
+     *
+     *
+     * getQuizzes test
+     *
+     *
+     */
+
+    @Test
+    public void testGetQuizzesExpect200() {
+        int pageIdParam = 1;
+        int defaultPageSize = 10;
+
+        Pageable paging = PageRequest.of(pageIdParam, defaultPageSize);
+        Mockito.when(quizService.findAll(paging)).thenReturn(null);
+
+        assertEquals(HttpStatus.OK, quizController.getQuizzes(pageIdParam));
+    }
 }
