c3d542b1c4e40076487b293773608f1ef8918abc
Mykhailo Bubnov <bubm02@vse.cz>
2022-10-27 00:31:23 +0200

    Added tests for getQuizzesCompletion, createQuiz, solveQuiz and deleteQuiz methods

--- Web Quiz Engine/task/test/tests/engine/QuizControllerTest.java
+++ Web Quiz Engine/task/test/tests/engine/QuizControllerTest.java
@@ -1,11 +1,13 @@
 package tests.engine;
 
 
+import engine.JSONEntities.Answer;
 import engine.JSONEntities.User;
 import engine.QuizController;
 import engine.database.quizcompletiontable.QuizCompletionService;
 import engine.database.quiztable.QuizJPAEntity;
 import engine.database.quiztable.QuizService;
+import engine.database.usertable.UserJPAEntity;
 import engine.database.usertable.UserService;
 import engine.exceptions.BadRegistrationRequestException;
 import engine.exceptions.QuizNotFoundException;
@@ -14,6 +16,7 @@ import org.mockito.Mockito;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,9 +26,10 @@ import static org.mockito.ArgumentMatchers.any;
 
 public class QuizControllerTest {
 
-    private QuizService quizService = Mockito.mock(QuizService.class);
-    private UserService userService = Mockito.mock(UserService.class);
-    private QuizCompletionService quizCompletionService = Mockito.mock(QuizCompletionService.class);
+    private final QuizService quizService = Mockito.mock(QuizService.class);
+    private final UserService userService = Mockito.mock(UserService.class);
+    private final QuizCompletionService quizCompletionService = Mockito.mock(QuizCompletionService.class);
+    private final Authentication auth = Mockito.mock(Authentication.class);
 
     private final QuizController quizController = new QuizController(quizService, userService, quizCompletionService);
 
@@ -35,7 +39,7 @@ public class QuizControllerTest {
 
 
     }
-    /**
+    /*
      *
      *
      *
@@ -113,7 +117,7 @@ public class QuizControllerTest {
     }
 
 
-    /**
+    /*
      *
      *
      * getQuiz test
@@ -138,22 +142,256 @@ public class QuizControllerTest {
     }
 
 
-    /**
+    /*
      *
      *
-     * getQuizzes test
+     * getQuizzes tests
      *
      *
      */
 
     @Test
-    public void testGetQuizzesExpect200() {
+    public void testGetQuizzes_Expect200() {
         int pageIdParam = 1;
         int defaultPageSize = 10;
 
         Pageable paging = PageRequest.of(pageIdParam, defaultPageSize);
         Mockito.when(quizService.findAll(paging)).thenReturn(null);
 
-        assertEquals(HttpStatus.OK, quizController.getQuizzes(pageIdParam));
+        assertEquals(HttpStatus.OK, quizController.getQuizzes(pageIdParam).getStatusCode());
+    }
+
+
+    /*
+     *
+     *
+     * getQuizzesCompletion tests
+     *
+     *
+     */
+
+    @Test
+    public void testGetQuizzesCompletion_Expect200() {
+        int pageIdParam = 1;
+        int defaultPageSize = 10;
+
+        Pageable paging = PageRequest.of(pageIdParam, defaultPageSize);
+        Mockito.when(quizService.findAll(paging)).thenReturn(null);
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizCompletionService.findQuizCompletionEntitiesByUserId(
+                Mockito.any(Pageable.class),
+                Mockito.any(UserJPAEntity.class)))
+                .thenReturn(null);
+
+        assertEquals(HttpStatus.OK,
+                quizController.getQuizzesCompletion(pageIdParam, auth).getStatusCode());
+    }
+
+    /*
+     *
+     *
+     * createQuiz tests
+     *
+     *
+     */
+
+    @Test
+    public void testCreateQuiz_Expect201() {
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.saveQuiz(Mockito.any(QuizJPAEntity.class))).thenReturn(new QuizJPAEntity());
+
+        QuizJPAEntity quiz = new QuizJPAEntity();
+        quiz.setTitle("The Java Logo");
+        quiz.setText("What is depicted on the Java logo?");
+        quiz.setOptions(new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"});
+        quiz.setAnswer(new int[]{2,3});
+
+        assertEquals(HttpStatus.CREATED,
+                quizController.createQuiz(new QuizJPAEntity(), auth).getStatusCode());
+    }
+
+    /*
+     *
+     *
+     * solveQuiz tests
+     *
+     *
+     */
+
+    @Test
+    public void testSolveQuiz_GivenValidAnswer_Expect200() {
+        int testQuizId = 1;
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setAnswer(new int[]{1,2});
+
+        Answer testAnswer = new Answer();
+        testAnswer.setAnswer(new int[]{1, 2});
+
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+
+        assertEquals(HttpStatus.OK,
+                quizController.solveQuiz(testQuizId, testAnswer, auth).getStatusCode());
+    }
+
+    @Test
+    public void testSolveQuiz_GivenValidNullAnswer_Expect200() {
+        int testQuizId = 1;
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setAnswer(null);
+
+        Answer testAnswer = new Answer();
+
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+
+        assertEquals(HttpStatus.OK,
+                quizController.solveQuiz(testQuizId, testAnswer, auth).getStatusCode());
+    }
+
+    @Test
+    public void testSolveQuiz_GivenValidBlankAnswer_Expect200() {
+        int testQuizId = 1;
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setAnswer(new int[]{});
+
+        Answer testAnswer = new Answer();
+        testAnswer.setAnswer(new int[]{});
+
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+
+        assertEquals(HttpStatus.OK,
+                quizController.solveQuiz(testQuizId, testAnswer, auth).getStatusCode());
+    }
+
+    @Test
+    public void testSolveQuiz_GivenInvalidAnswer_Expect400() {
+        int testQuizId = 1;
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setAnswer(new int[]{1,2});
+
+        Answer testAnswer = new Answer();
+        testAnswer.setAnswer(new int[]{3,4});
+
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+
+        assertEquals(HttpStatus.BAD_REQUEST,
+                quizController.solveQuiz(testQuizId, testAnswer, auth).getStatusCode());
+    }
+
+    @Test
+    public void testSolveQuiz_GivenHalfValidAnswer_Expect400() {
+        int testQuizId = 1;
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setAnswer(new int[]{1,2});
+
+        Answer testAnswer = new Answer();
+        testAnswer.setAnswer(new int[]{1});
+
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+
+        assertEquals(HttpStatus.BAD_REQUEST,
+                quizController.solveQuiz(testQuizId, testAnswer, auth).getStatusCode());
+    }
+
+    @Test
+    public void testSolveQuiz_GivenInvalidQuizId_ExpectThrows() {
+        int testQuizId = 1;
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setAnswer(new int[]{1, 2});
+
+        Answer testAnswer = new Answer();
+        testAnswer.setAnswer(new int[]{1, 2});
+
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(false);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(null);
+
+        assertThrows(QuizNotFoundException.class,
+                () -> quizController.solveQuiz(testQuizId, testAnswer, auth));
+    }
+
+    /*
+     *
+     *
+     * deleteQuiz tests
+     *
+     *
+     */
+
+    @Test
+    public void testDeleteQuiz_GivenValidQuizId_Expect204() {
+        int testQuizId = 1;
+        int testUserQuizDeleterId = 1;
+
+        UserJPAEntity testUserQuizDeleter = new UserJPAEntity();
+        testUserQuizDeleter.setId(testUserQuizDeleterId);
+
+        UserJPAEntity testUserQuizCreator = new UserJPAEntity();
+        testUserQuizCreator.setId(testQuizId);
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setId(testQuizId);
+        testQuiz.setUser(testUserQuizCreator);
+
+        Mockito.when(auth.getPrincipal()).thenReturn(testUserQuizDeleter);
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+        assertEquals(HttpStatus.NO_CONTENT,
+                quizController.deleteQuiz(testQuizId, auth).getStatusCode());
+    }
+
+    @Test
+    public void testDeleteQuiz_GivenInvalidQuizId_Expect403() {
+        int testQuizId = 2;
+        int testUserQuizDeleterId = 1;
+
+        UserJPAEntity testUserQuizDeleter = new UserJPAEntity();
+        testUserQuizDeleter.setId(testUserQuizDeleterId);
+
+        UserJPAEntity testUserQuizCreator = new UserJPAEntity();
+        testUserQuizCreator.setId(testQuizId);
+
+        QuizJPAEntity testQuiz = new QuizJPAEntity();
+        testQuiz.setId(testQuizId);
+        testQuiz.setUser(testUserQuizCreator);
+
+        Mockito.when(auth.getPrincipal()).thenReturn(testUserQuizDeleter);
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(true);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(testQuiz);
+
+        assertEquals(HttpStatus.FORBIDDEN,
+                quizController.deleteQuiz(testQuizId, auth).getStatusCode());
+    }
+
+    @Test
+    public void testDeleteQuiz_GivenNonexistentQuiz_ExpectThrows() {
+        int testQuizId = 1;
+        Mockito.when(auth.getPrincipal()).thenReturn(new UserJPAEntity());
+        Mockito.when(quizService.isQuizExist(testQuizId)).thenReturn(false);
+        Mockito.when(quizService.getQuizById(testQuizId)).thenReturn(new QuizJPAEntity());
+
+        assertThrows(QuizNotFoundException.class,
+                () -> quizController.deleteQuiz(testQuizId, auth));
     }
 }
